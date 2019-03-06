b4672a9b3580a5b15c632b2527ad3adc6129437d
MULTIJACKER <38036342+MULTIJACKER@users.noreply.github.com>
2019-03-06 12:04:23 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,10 +5,11 @@ import document from "document";
 import { HeartRateSensor } from "heart-rate";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
+//import * as hrm from "hrm.js";
 
 
 // Update the clock every second
-//clock.granularity = "seconds";
+clock.granularity = "seconds";
 
 // Get a handle on the <text> element
 const bps = new BodyPresenceSensor();
@@ -17,14 +18,11 @@ const hrm = new HeartRateSensor();
 // Starts the sensors
 bps.start();
 hrm.start();
+//hrm.initialize();
 
 const bpsData = document.getElementById("bps-data");
 let dat = document.getElementById("dat");
-let hrmDataGreen = document.getElementById("hrm-data-green");
-let hrmDataYellow = document.getElementById("hrm-data-yellow");
-let hrmDataOrange = document.getElementById("hrm-data-orange");
-let hrmDataRed = document.getElementById("hrm-data-red");
-let hrmDataGray = document.getElementById("hrm-data-gray");
+let hrmData = document.getElementById("hrm-data");
 const Time = document.getElementById("Time");
 const TimeSec = document.getElementById("TimeSec");
 
@@ -32,6 +30,7 @@ const TimeSec = document.getElementById("TimeSec");
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) =>
 {
+  
   let today = evt.date;
   let hours = util.zeroPad(today.getHours());
   let mins = util.zeroPad(today.getMinutes());
@@ -49,73 +48,50 @@ clock.ontick = (evt) =>
       hrm:
       {
         heartRate: hrm.heartRate ? hrm.heartRate : 0
-      }  
+      }
     }
   }
 
   refreshData();
-  setInterval(refreshData, 500);  
+  setInterval(refreshData, 1000);
   
   Time.text = `${hours}:${mins}`;
   TimeSec.text = `${secs}`;
 
   if (hrm.heartRate != null && bps.present == 1)
   {
-    //hrmData.text = hrm.heartRate;
     switch (util.fancyHR(hrm.heartRate))
     {
         case 0:
-            hrmDataGreen.text = hrm.heartRate;
-            hrmDataYellow.text = "";
-            hrmDataOrange.text = "";
-            hrmDataRed.text = "";
-            hrmDataGray.text = "";
+            hrmData.text = hrm.heartRate;
+            hrmData.style.fill = "green";
             break;
         case 1:
-            hrmDataYellow.text = hrm.heartRate;
-            hrmDataGreen.text = "";
-            hrmDataOrange.text = "";
-            hrmDataRed.text = "";
-            hrmDataGray.text = "";
+            hrmData.text = hrm.heartRate;
+            hrmData.style.fill = "yellow";
             break;
         case 2:
-            hrmDataOrange.text = hrm.heartRate;
-            hrmDataGreen.text = "";
-            hrmDataYellow.text = "";
-            hrmDataRed.text = "";
-            hrmDataGray.text = "";
+            hrmData.text = hrm.heartRate;
+            hrmData.style.fill = "orange";
             break;
         case 3:
-            hrmDataRed.text = hrm.heartRate;
-            hrmDataGreen.text = "";
-            hrmDataYellow.text = "";
-            hrmDataOrange.text = "";
-            hrmDataGray.text = "";
+            hrmData.text = hrm.heartRate;
+            hrmData.style.fill = "red";
             break;
         case 4:
-            hrmDataGray.text = hrm.heartRate;
-            hrmDataGreen.text = "";
-            hrmDataYellow.text = "";
-            hrmDataOrange.text = "";
-            hrmDataRed.text = "";
+            hrmData.text = "--";
+            hrmData.style.fill = "gray";
             break;
         default:
-            hrmDataGray.text = "--";
-            hrmDataGreen.text = "";
-            hrmDataYellow.text = "";
-            hrmDataOrange.text = "";
-            hrmDataRed.text = "";
+            hrmData.text = "--";
+            hrmData.style.fill = "gray";
     }
   }
   else
   {
-    //Gray
-    hrmDataGray.text = "--";
-    hrmDataGreen.text = "";
-    hrmDataYellow.text = "";
-    hrmDataOrange.text = "";
-    hrmDataRed.text = "";
+    hrmData.text = "--";
+    hrmData.style.fill = "gray";
   }
-
+  
   dat.text = util.fancyDate(finalDate);
 }
